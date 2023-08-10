1c56ff0baf911fbd48216816076c8c63effdea09
KCH <130543414+kkkkcccchhhh@users.noreply.github.com>
2023-08-10 19:08:28 +0900

    불필요한 주석 삭제, npm install --save-dev nodemon 설치 완료

--- changhyun/app.js
+++ changhyun/app.js
@@ -9,12 +9,12 @@ const app = express();
 
 
 const appDataSource = new DataSource({
- type: process.env.DB_CONNECTION,
- host: process.env.DB_HOST,
- port: process.env.DB_PORT,
- username: process.env.DB_USERNAME,
- password: process.env.DB_PASSWORD,
- database: process.env.DB_DATABASE
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  ddatabase: process.env.DB_DATABASE
 })
 
 appDataSource.initialize()
